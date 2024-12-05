3ee925608f7a2a4aa256b2a0a71307748185f09a
Foxichigo <linseyle.ll@gmail.com>
2024-12-05 18:04:07 -0500

    Quicksand

--- cdan_fa24_action1/Assets/Scripts/DamageSlow.cs
+++ cdan_fa24_action1/Assets/Scripts/DamageSlow.cs
@@ -10,31 +10,51 @@ public class DamageSlow : MonoBehaviour {
        private float damageTimer = 0f;
        private GameHandler gameHandlerObj;
 
+       //quiksand:
+       private Vector2 playerVelocityStart;
+       private Vector2 playerVelocityHalf;
+       public float qsAmt = 4f;
+       public GameObject pullOutSandPS;
+
        void Start () {
          if (GameObject.FindWithTag ("GameHandler") != null) {
-            gameHandlerObj = GameObject.FindWithTag ("GameHandler").GetComponent<GameHandler>();
+            gameHandlerObj = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
          }
        }
 
        void FixedUpdate(){
-              if (isDamaging == true){
-                     damageTimer += 0.1f;
-                     if (damageTimer >= damageTime){
-                            gameHandlerObj.playerGetHit (damage);
-                            damageTimer = 0f;
-                     }
+          if (isDamaging == true){
+              damageTimer += 0.1f;
+              if (damageTimer >= damageTime){
+                Debug.Log("I AM DAMAGING YOU!!! In Quicksand");
+                gameHandlerObj.playerGetHit(damage);
+                damageTimer = 0f;
               }
+          }
        }
 
        void OnTriggerStay2D(Collider2D other){
               if (other.gameObject.tag == "Player") {
                      isDamaging = true;
+                     playerVelocityStart = other.gameObject.GetComponent<Rigidbody2D>().velocity;
+                     playerVelocityHalf = new Vector2(playerVelocityStart.x/qsAmt,playerVelocityStart.y/qsAmt);
+                     other.gameObject.GetComponent<Rigidbody2D>().velocity = playerVelocityHalf;
               }
        }
 
        void OnTriggerExit2D(Collider2D other){
               if (other.gameObject.tag == "Player") {
                      isDamaging = false;
+                     other.gameObject.GetComponent<Rigidbody2D>().velocity = playerVelocityStart;
+                     GameObject sandPS = Instantiate (pullOutSandPS, other.gameObject.transform.position, Quaternion.identity);
+                     StartCoroutine(DestroyParticles(sandPS));
               }
        }
+
+
+       IEnumerator DestroyParticles(GameObject PS){
+         yield return new WaitForSeconds(1f);
+         Destroy(PS);
+       }
+
 }
